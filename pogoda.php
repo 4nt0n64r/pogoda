af2bc6f82aa323b7cd39204ea170188ac303e21a
Anton Sharapov <cheel4069@gmail.com>
2019-02-02 20:36:20 +0200

    Initial commit

---
+++ pogoda.php
@@ -0,0 +1,21 @@
+[
+  {
+    "date":"2019-02-02",
+    "tod":"0",
+    "pressure":"699",
+    "temp":"?21",
+    "feel":"?27",
+    "humidity":"39",
+    "wind":"\u0421\u0417 3\u043c\/\u0441",
+    "cloud":"\u042f\u0441\u043d\u043e",
+    "tid":"74"
+  },
+  {"date":"2019-02-02","tod":"1","pressure":"703","temp":"?27","feel":"?31","humidity":"61","wind":"\u0412 1\u043c\/\u0441","cloud":"\u041e\u0431\u043b\u0430\u0447\u043d\u043e","tid":"74"},
+  {"date":"2019-02-02","tod":"2","pressure":"706","temp":"?18","feel":"?24","humidity":"42","wind":"\u0421\u0417 4\u043c\/\u0441","cloud":"\u041e\u0431\u043b\u0430\u0447\u043d\u043e","tid":"74"},
+  {"date":"2019-02-02","tod":"3","pressure":"710","temp":"?24","feel":"?29","humidity":"53","wind":"\u0417 3\u043c\/\u0441","cloud":"\u042f\u0441\u043d\u043e","tid":"74"},
+  {"date":"2019-02-03","tod":"0","pressure":"711","temp":"?29","feel":"?35","humidity":"52","wind":"\u0417 3\u043c\/\u0441","cloud":"\u041e\u0431\u043b\u0430\u0447\u043d\u043e","tid":"74"},
+  {"date":"2019-02-03","tod":"1","pressure":"710","temp":"?31","feel":"?35","humidity":"56","wind":"\u0417 2\u043c\/\u0441","cloud":"\u042f\u0441\u043d\u043e","tid":"74"},
+  {"date":"2019-02-03","tod":"2","pressure":"708","temp":"?23","feel":"?29","humidity":"40","wind":"\u0417 4\u043c\/\u0441","cloud":"\u041f\u0430\u0441\u043c\u0443\u0440\u043d\u043e","tid":"74"},
+  {"date":"2019-02-03","tod":"3","pressure":"706","temp":"?25","feel":"?25","humidity":"42","wind":"\u0428 0\u043c\/\u0441","cloud":"\u041e\u0431\u043b\u0430\u0447\u043d\u043e","tid":"74"},
+  {"date":"2019-02-04","tod":"0","pressure":"704","temp":"?27","feel":"?28","humidity":"55","wind":"\u0412 1\u043c\/\u0441","cloud":"\u041f\u0430\u0441\u043c\u0443\u0440\u043d\u043e","tid":"74"}
+]
